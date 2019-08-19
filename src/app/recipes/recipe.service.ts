1b18306c706182798d6b69c5f603d58367265ffa
PranavBhatia <bhatiapranav1996@gmail.com>
2019-08-19 15:53:34 -0400

    Managing Recipes in a Recipe Service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,14 @@
-export class RecipeService {
+import {Recipe} from './recipe.model';
 
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('Test Recipe 1', 'A dummy kind of description', 'https://www.bbcgoodfood.com/sites/default/files/' +
+      'categories/categories-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
+    new Recipe('Test Recipe 2', 'A dummy kind of description', 'https://www.bbcgoodfood.com/sites/default/files/' +
+      'categories/categories-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();  // so that we dont get the actual array object, just get a copy
+  }
 }
