7bf6c9638149e26acc4d288a1f6714b4e022d3c8
PranavBhatia <bhatiapranav1996@gmail.com>
2020-03-04 09:54:04 -0500

    Transforming Response Data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -29,6 +30,16 @@ export class DataStorageService {
   fetchRecipes() {
     this.http
       .get<Recipe[]>('https://recipes-and-shopping-8b416.firebaseio.com/recipes.json')
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        })
+      )
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
       });
