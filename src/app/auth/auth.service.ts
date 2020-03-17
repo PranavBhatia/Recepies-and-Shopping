30c619389c58889bf80a8b252645e12ee07460a0
PranavBhatia <bhatiapranav1996@gmail.com>
2020-03-17 10:56:30 -0400

    Login Error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {catchError} from 'rxjs/operators';
 import {throwError} from 'rxjs';
 
@@ -20,61 +20,59 @@ export class AuthService {
   constructor(private httpClient: HttpClient) {
   }
 
+  private static handleError(errorResponse: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorResponse.error || !errorResponse.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorResponse.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'The email address is already in use by another account.';
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = 'Password sign-in is disabled for this project';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'The password is invalid or the user does not have a password.';
+        break;
+      case 'USER_DISABLED':
+        errorMessage = 'The user account has been disabled by an administrator.';
+        break;
+    }
+    return throwError(errorMessage);
+  }
+
   signUp(email: string, password: string) {
     const apiKey = 'AIzaSyDQSNbZxTl62NUdkkxO2Y_ZlVPiPltiXc4';
-    return this.httpClient.post<AuthResponseData>(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`,
-      {
-        email,
-        password,
-        returnSecureToken: true
-      }
-    ).pipe(catchError(errorResponse => {
-      let errorMessage = 'An unknown error occured!';
-      if (!errorResponse.error || !errorResponse.error.error) {
-        return throwError(errorMessage);
-      }
-      switch (errorResponse.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = 'The email address is already in use by another account.';
-          break;
-        case 'OPERATION_NOT_ALLOWED':
-          errorMessage = 'Password sign-in is disabled for this project';
-          break;
-        case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-          errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
-          break;
-      }
-      return throwError(errorMessage);
-    }));
+    return this.httpClient
+      .post<AuthResponseData>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`,
+        {
+          email,
+          password,
+          returnSecureToken: true
+        }
+      )
+      .pipe(catchError(AuthService.handleError));
   }
 
   login(email: string, password: string) {
     const apiKey = 'AIzaSyDQSNbZxTl62NUdkkxO2Y_ZlVPiPltiXc4';
-    return this.httpClient.post<AuthResponseData>(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`,
-      {
-        email,
-        password,
-        returnSecureToken: true
-      }
-    ).pipe(catchError(errorResponse => {
-      let errorMessage = 'An unknown error occured!';
-      if (!errorResponse.error || !errorResponse.error.error) {
-        return throwError(errorMessage);
-      }
-      switch (errorResponse.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = 'The email address is already in use by another account.';
-          break;
-        case 'OPERATION_NOT_ALLOWED':
-          errorMessage = 'Password sign-in is disabled for this project';
-          break;
-        case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-          errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
-          break;
-      }
-      return throwError(errorMessage);
-    }));
+    return this.httpClient
+      .post<AuthResponseData>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`,
+        {
+          email,
+          password,
+          returnSecureToken: true
+        }
+      )
+      .pipe(catchError(AuthService.handleError));
   }
 }
